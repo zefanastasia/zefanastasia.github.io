c4ac5aed42ca9ee7afd576a87fc88c18d04cb319
EWS Guest EWS Guest <ewsguest280@siebl-0222-15.ews.illinois.edu>
2018-11-11 11:44:47 -0600

    i made a website

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+ function clickFunction() {
+   //tell the website what to do when a user clicks on button
+   document.getElementById("display_text").innerHTML = "Welcome to History Fair Project!";
+
+ }
